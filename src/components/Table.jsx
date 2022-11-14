c3654c6affcd928b019590fb7f81e7bee17efb88
unknown <peacesandy04@gmail.com>
2022-11-14 12:00:44 -0800

    fixed a bug

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,17 +3,21 @@ import React from 'react'
 function Table({countries}) {
   
   const renderCountries = () => {
-    const countryItems = countries && countries.map((country, id) => { 
-    <tr>
+    const countryItems = countries && countries.map((country, id) => {
+      const tr =  
+    <tr key={id}>
       <td>{id}</td>
       <td>{country.name}</td>
       <td>{country.capital}</td>
       <td>{country.population}</td>
       <td>{country?.currencies?.[0]?.symbol}</td>
-      <td><img src={country.flag} alt="country-flag" srcset="" /></td>
-    </tr>} )
+      <td><img src={country.flag} alt="country-flag" className='img' /></td>
+    </tr>
+      return tr
+  } )
       return countryItems
   }
+  console.log(renderCountries())
   return (
     <table className='table'>
         <thead>
